238604c5ae25b7444845a140a2113e5c31088281
Alexandra <stannida@gmail.com>
2019-05-16 15:54:23 +0200

    updating setup.py for the smm wrapper

--- setup.py
+++ setup.py
@@ -10,26 +10,26 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wikiwho_wrapper",
-    version="1.6.2",
+    name="smm_wrapper",
+    version="1.0",
     # Author details
     author="",
-    author_email="wikiwho@gesis.org",
-    description="A light/simple Python wrapper for the WikiWho API",
+    author_email="ulloaro@gesis.org",
+    description="A light Python wrapper for the SMM API (Politicians Social Media Monitoring API)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gesiscss/wikiwho_wrapper",
+    url="https://github.com/gesiscss/smm_wrapper",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
     # What does your project relate to?
-    keywords='wikipedia wikiwho revisions content authorship',
+    keywords='social media smm politicians',
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'pandas', 'deprecation', 'wikiwho_pickle']
+    install_requires=['requests', 'pandas', 'deprecation']
 )
